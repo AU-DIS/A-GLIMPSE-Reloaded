8574cf7d23194b3b4b0ee31cfb0ccb64c2b0b62f
Mathias Ravn Tversted <Mathias@thesun.dk>
2021-02-26 17:01:58 +0100

    Multi-level bandits & test

--- exp3.py
+++ exp3.py
@@ -23,7 +23,7 @@ class exp3_bandit(object):
         self.reward_max = 1
         self.round = 0
         self.distribution = [1.0] * number_of_triples
-        self.choice = random.randint(0, number_of_triples)
+        self.choice = 0
         self.choices = choices
         self.gamma = 0.07
         self.leaf = False
@@ -35,15 +35,14 @@ class exp3_bandit(object):
 
     # How much you get if you select this (adding) (difference in reward)
     # Marginal gain (phi is from eq 2)
-    def give_reward(self, reward):
+    def give_reward(self, reward, c):
         global reward_max, reward_min
         scaled_reward = (reward - self.reward_min) / \
             (self.reward_max - self.reward_min)
 
-
-        estimated_reward = 1.0 * scaled_reward / self.distribution[self.choice]
-        self.weights[self.choice] *= math.exp(estimated_reward *
-                                              self.gamma / self.k)
+        estimated_reward = 1.0 * scaled_reward / self.distribution[c]
+        self.weights[c] *= math.exp(estimated_reward *
+                                    self.gamma / self.k)
 
 
 # draw: [float] -> int
@@ -72,19 +71,20 @@ def distr(weights, gamma=0.0):
 
 
 class recursive_exp3(object):
-    def __init__(self, choice_start, choice_end):
+    def __init__(self, choice_start, choice_end, triples):
         global split_constant
         self.choice_start = choice_start
         self.choice_end = choice_end
         #split_constant = int((choice_end-choice_start)/split_percent)
         print(split_constant)
+        self.entities_chosen_by_bandit = dict()
         split_constant = int(math.sqrt(choice_end - choice_start))
 
         self.recursive_bandits = []
 
         # Leaf decisions
         choices = np.array_split(
-            np.arange(choice_start, choice_end, step=1), split_constant)
+            (np.arange(0, choice_start + choice_end - choice_start, step=1)), split_constant)
 
         print(len(choices[0]))
         for i in range(split_constant):
@@ -94,13 +94,39 @@ class recursive_exp3(object):
 
         self.bandit = exp3_bandit(split_constant, self.recursive_bandits)
 
-    def make_choices(self, k):
-        choices = set()
+    def make_choices(self, k, kg):
+        choices = []
+        c_values = []
         while len(choices) <= k:
-            _, b, _ = self.bandit.choose_triple()
-            _, t, _ = b.choose_triple()
-            choices.add(t)
-        return choices
+            bandit_index1, b, _ = self.bandit.choose_triple()
+            c, t, _ = b.choose_triple()
+            # Need to extract triples here //TODO: We have indices
+            if t not in choices:
+                choices.append(t)
+        choices = kg.get_triples(choices)
 
-    def give_reward(self):
-        pass
+        for c, cho in zip(c_values, choices):
+            (e1, _, e2) = cho
+            if e1 not in self.entities_chosen_by_bandit.keys():
+                self.entities_chosen_by_bandit[e1] = [(bandit_index1, c)]
+            else:
+                self.entities_chosen_by_bandit[e2].append(
+                    (bandit_index1, c))
+            if e2 not in self.entities_chosen_by_bandit.keys():
+                self.entities_chosen_by_bandit[e2] = [(bandit_index1, c)]
+            else:
+                self.entities_chosen_by_bandit[e2].append(
+                    (bandit_index1, c))
+
+        return set(choices)
+
+    def give_reward(self, queries):
+
+        for entity in self.entities_chosen_by_bandit.keys():
+            for (bandit_index, choice) in self.entities_chosen_by_bandit[entity]:
+                if entity in queries:
+                    self.recursive_bandits[bandit_index].give_reward(1, choice)
+                    self.bandit.give_reward(1, bandit_index)
+                else:
+                    self.recursive_bandits[bandit_index].give_reward(0, choice)
+                    self.bandit.give_reward(0, bandit_index)
